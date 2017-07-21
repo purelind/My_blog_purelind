47415abb472bfb76a7a2b64fdc74070854789dfe
purelind <kevinsocial@outlook.com>
2017-07-21 09:45:23 +0800

    creating a migration repository

--- manage.py
+++ manage.py
@@ -3,15 +3,16 @@ import os
 from app import create_app, db
 from app.models import User, Role, Post
 from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
-
+migrate = Migrate(app, db)
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role, Post=Post)
 manager.add_command("shell", Shell(make_context=make_shell_context))
-
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test():
