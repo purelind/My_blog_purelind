849e71d10528f41a03c4fbebf3eddc01de5a0c8d
purelind <kevinsocial@outlook.com>
2017-07-21 11:25:38 +0800

    heroku config

--- manage.py
+++ manage.py
@@ -9,10 +9,12 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
+
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role, Post=Post)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+
 
 @manager.command
 def test():
@@ -21,6 +23,13 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+
+@manager.command
+def deploy():
+    """Run deployment tasks."""
+    from flask_migrate import upgrade
+
+    upgrade()
 
 if __name__ == '__main__':
     manager.run()
