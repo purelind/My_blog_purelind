4ceaae9dd130ce4e18a11fa46e38fadee6c6382a
purelind <kevinsocial@outlook.com>
2017-07-21 09:06:37 +0800

    Initial implementation

---
+++ app/main/views.py
@@ -0,0 +1,20 @@
+from flask import render_template
+from ..models import Post
+from . import main
+
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', posts=posts)
+
+
+@main.route('/post/<int:index>', methods=['GET'])
+def post(index):
+    post = Post.query.get_or_404(index)
+    return render_template('post.html', post=post)
+
+
+@main.route('/about', methods=['GET'])
+def about_site():
+    return render_template('about.html')
---
+++ app/models.py
@@ -0,0 +1,50 @@
+from . import db
+from datetime import datetime
+from markdown import markdown
+import bleach
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64))
+    body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
+    summury = db.Column(db.Text)
+    summury_html = db.Column(db.Text)
+
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'code', 'blockquote', 'em', 'i',
+                        'strong', 'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True)
+        )
+
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
+
+
